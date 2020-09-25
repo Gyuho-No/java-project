e11c89bf2f7cb729f1c49a659b1e2b01fcf5e337
Gyuho-No <roh213589@naver.com>
2020-09-25 17:44:28 +0900

    java 프로젝트 시작

---
+++ java_project/src/java_project/ClassExam.java
@@ -0,0 +1,12 @@
+package java_project;
+
+public class ClassExam {
+
+	public static void main(String[] args) {
+		
+		System.out.println("hello git");
+		// TODO Auto-generated method stub
+
+	}
+
+}
