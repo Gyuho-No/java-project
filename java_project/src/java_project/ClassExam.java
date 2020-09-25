119f73e0d4d1fde85392c1fc6da5eddc7160be3b
Gyuho-No <roh213589@naver.com>
2020-09-25 17:45:06 +0900

    modify 출력 문자열 변경

--- java_project/src/java_project/ClassExam.java
+++ java_project/src/java_project/ClassExam.java
@@ -4,7 +4,7 @@ public class ClassExam {
 
 	public static void main(String[] args) {
 		
-		System.out.println("hello git");
+		System.out.println("hello eclipse");
 		// TODO Auto-generated method stub
 
 	}
